0ae43d152f1dc4352f59ed536fb4e303d0d29606
githunman <githubman@gmail.com>
2012-03-28 16:33:43 +0200

    added test project

---
+++ Tests/ProfileServiceTest.cs
@@ -0,0 +1,92 @@
+﻿using SampleAppWithNugetDependency;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Tests
+{
+    
+    
+    /// <summary>
+    ///This is a test class for ProfileServiceTest and is intended
+    ///to contain all ProfileServiceTest Unit Tests
+    ///</summary>
+    [TestClass()]
+    public class ProfileServiceTest
+    {
+
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        // 
+        //You can use the following additional attributes as you write your tests:
+        //
+        //Use ClassInitialize to run code before running the first test in the class
+        //[ClassInitialize()]
+        //public static void MyClassInitialize(TestContext testContext)
+        //{
+        //}
+        //
+        //Use ClassCleanup to run code after all tests in a class have run
+        //[ClassCleanup()]
+        //public static void MyClassCleanup()
+        //{
+        //}
+        //
+        //Use TestInitialize to run code before running each test
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{
+        //}
+        //
+        //Use TestCleanup to run code after each test has run
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{
+        //}
+        //
+        #endregion
+
+
+        /// <summary>
+        ///A test for ProfileService Constructor
+        ///</summary>
+        [TestMethod()]
+        public void ProfileServiceConstructorTest()
+        {
+            ProfileService target = new ProfileService();
+            Assert.Inconclusive("TODO: Implement code to verify target");
+        }
+
+        /// <summary>
+        ///A test for Profile
+        ///</summary>
+        [TestMethod()]
+        public void ProfileTest()
+        {
+            ProfileService target = new ProfileService(); // TODO: Initialize to an appropriate value
+            int result = 0; // TODO: Initialize to an appropriate value
+            int expected = 0; // TODO: Initialize to an appropriate value
+            int actual;
+            actual = target.Profile(result);
+            Assert.AreEqual(expected, actual);
+            Assert.Inconclusive("Verify the correctness of this test method.");
+        }
+    }
+}
